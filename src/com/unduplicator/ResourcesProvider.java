dacb729281a008d79e55c5d10b156681d7ed9ea1
MontolioV <vetal07_mail@mail.ru>
2017-08-19 15:20:52 +0300

    Resource singleton.

--- src/com/unduplicator/ResourcesProvider.java
+++ src/com/unduplicator/ResourcesProvider.java
@@ -1,22 +1,82 @@
 package com.unduplicator;
 
-import java.util.ResourceBundle;
-import java.util.concurrent.Semaphore;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * <p>Created by MontolioV on 18.08.17.
  */
 public class ResourcesProvider {
     private static ResourcesProvider ourInstance = new ResourcesProvider();
-    private ResourceBundle exceptionsBundle;
-    private ResourceBundle guiBundle;
-    private ResourceBundle messagesBundle;
-    private Semaphore semaphore = new Semaphore(1);
+    private Locale currentLocal;
+    private Map<String, ResourceBundle> bundles = new HashMap<>();
+    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     public static ResourcesProvider getInstance() {
         return ourInstance;
     }
 
     private ResourcesProvider() {
+        currentLocal = getSavedLocale();
+        setBundlesLocale(currentLocal);
+    }
+
+    protected Locale getSavedLocale() {
+        try (ObjectInputStream ois = new ObjectInputStream(
+                                     new FileInputStream(System.getProperty("user.dir") + "/locale.ser")))
+        {
+            return (Locale) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new Locale("en", "EN");
+        }
+    }
+
+    protected boolean saveLocale() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                                      new FileOutputStream(System.getProperty("user.dir") + "/locale.ser")))
+        {
+            oos.writeObject(currentLocal);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    protected void setBundlesLocale(Locale locale) {
+        currentLocal = locale;
+
+        readWriteLock.writeLock().lock();
+
+        ResourceBundle exceptionsBundle = ResourceBundle.getBundle("com.resources.GUI_Bundle", locale);
+        ResourceBundle guiBundle = ResourceBundle.getBundle("com.resources.GUI_Bundle", locale);
+        ResourceBundle messagesBundle = ResourceBundle.getBundle("com.resources.Messages_Bundle", locale);
+        bundles.put("exceptions", exceptionsBundle);
+        bundles.put("gui", guiBundle);
+        bundles.put("messages", messagesBundle);
+
+        readWriteLock.writeLock().unlock();
+    }
+
+    public String getStrFromBundle(String bundleName, String stringKey) {
+        readWriteLock.readLock().lock();
+        String result = bundles.get(bundleName).getString(stringKey);
+        readWriteLock.readLock().unlock();
+
+        return result;
+    }
+
+    String getStrFromExceptionBundle(String stringKey) {
+        return getStrFromBundle("exceptions", stringKey);
+    }
+
+    String getStrFromGUIBundle(String stringKey) {
+        return getStrFromBundle("gui", stringKey);
+    }
+
+    String getStrFromMessagesBundle(String stringKey) {
+        return getStrFromBundle("messages", stringKey);
     }
 }
